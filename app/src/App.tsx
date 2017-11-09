34d7fdbe101aef2fd890f04e755bfa1f569f2d6d
Tom Lagier <tom@gliffy.com>
2017-11-09 13:31:39 -0800

    Ejected CRA, tweak for live reloading

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,7 +12,7 @@ class App extends React.Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          To get started, do a funky dance.
         </p>
       </div>
     );
